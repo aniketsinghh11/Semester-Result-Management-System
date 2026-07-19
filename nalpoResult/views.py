942f1a7d680abcecceb720591258f4815aa4587a
Aniket <asaniketsingh9@gmail.com>
2026-07-19 13:13:35 +0530

    add login,register,dashboard pages

--- nalpoResult/views.py
+++ nalpoResult/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
